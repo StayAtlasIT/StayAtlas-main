12cd74f93c7e4627262f8fdbca12d20f1c756d0b
StayAtlasIT <stayatlasit@gmail.com>
2025-08-21 19:14:48 +0530

    Updated App.jsx frontend

--- stayatlas-booking/src/App.jsx
+++ stayatlas-booking/src/App.jsx
@@ -22,7 +22,7 @@ import Profile from "./pages/Profile"
 import ViewExclusive from "./pages/ViewExclusive"
 import ExclusiveLayout from "./layout/ExclusiveLayout"
 import AdminDashboard from "./pages/AdminDashboard"
-import PropertyRequestForm from "./pages/listform"
+import PropertyRequestForm from "./pages/Listform"
 import AdminProtectedRoute from "./components/AdminProtectedRoute"
 import BookingDetailsPage from "./pages/BookingDetailsPage"
 import VerifyOTP from "./pages/VerifyOTP";
